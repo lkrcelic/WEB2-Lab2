5e73b981a533e7971be7907ba87604f04d3cdb0e
lkrcelic <lkrcelic@croz.net>
2023-11-13 01:16:55 +0100

    removed DB because I do not use it

--- src/main/java/web2/lab2/Lab2Application.java
+++ src/main/java/web2/lab2/Lab2Application.java
@@ -2,8 +2,9 @@ package web2.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class})
 public class Lab2Application {
 
     public static void main(String[] args) {
